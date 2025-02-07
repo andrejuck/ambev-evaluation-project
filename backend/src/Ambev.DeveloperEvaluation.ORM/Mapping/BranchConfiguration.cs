2a5d3812e1caa4a4db7e470a63d15ce75963a6c7
André Juck <andrejuck@hotmail.com>
2025-02-07 08:29:13 -0300

    Removing unnecessary usings

---
+++ backend/src/Ambev.DeveloperEvaluation.ORM/Mapping/BranchConfiguration.cs
@@ -0,0 +1,20 @@
+﻿
+using Ambev.DeveloperEvaluation.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ambev.DeveloperEvaluation.ORM.Mapping
+{
+    public class BranchConfiguration : IEntityTypeConfiguration<Branch>
+    {
+        public void Configure(EntityTypeBuilder<Branch> builder)
+        {
+            builder.ToTable("Branches");
+            builder.HasKey(p => p.Id);
+            builder.Property(u => u.Id).HasColumnType("uuid").HasDefaultValueSql("gen_random_uuid()");
+            builder.Property(p => p.Name)
+                .HasMaxLength(255)
+                .IsRequired();
+        }
+    }
+}
---
+++ backend/src/Ambev.DeveloperEvaluation.ORM/Mapping/ProductConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ambev.DeveloperEvaluation.ORM.Mapping
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.ToTable("Products");
+            builder.HasKey(p => p.Id);
+            builder.Property(u => u.Id).HasColumnType("uuid").HasDefaultValueSql("gen_random_uuid()");
+
+            builder.HasIndex(p => p.ProductNumber).IsUnique();
+            builder.Property(p => p.ProductNumber)
+                .IsRequired();
+            builder.Property(p => p.Name)
+                .HasMaxLength(255)
+                .IsRequired();
+            builder.Property(p => p.UnitPrice)
+                .HasColumnType("decimal(18,2)")
+                .IsRequired();
+        }
+    }
+}
